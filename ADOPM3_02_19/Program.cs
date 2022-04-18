da9370e1017f1c1a13939a6d883c104b1a5b8a49
Martin Lenart <martin_lenart@icloud.com>
2022-04-18 13:15:57 +0200

    Update

--- ADOPM3_02_19/Program.cs
+++ ADOPM3_02_19/Program.cs
@@ -12,22 +12,29 @@ namespace ADOPM3_02_19
 	public class Stock
 	{
 		string symbol;
-		decimal price;
 		public Stock(string symbol) { this.symbol = symbol; }
 		public event PriceChangedHandler PriceChanged; //Broadcaster event
+		public event PriceChangedHandler PriceChangedMoreThan20; //Broadcaster event
+
+		decimal _price;
 		public decimal Price
 		{
-			get { return price; }
+			get { return _price; }
 			set
 			{
-				if (price == value) return;         // Exit if nothing has changed
-				decimal oldPrice = price;
-				price = value;
+				if (_price == value) return;         // Exit if nothing has changed
+				decimal oldPrice = _price;
+				_price = value;
 
-				PriceChanged?.Invoke(oldPrice, price); // Invoke if not null
+				PriceChanged?.Invoke(oldPrice, _price); // Invoke if not null
 				// Above is excatly the same as
 				//if (PriceChanged != null)           
 					//PriceChanged(oldPrice, price);  // fire event.
+
+				if (_price >= oldPrice*1.20M)
+                {
+					PriceChangedMoreThan20?.Invoke(oldPrice, _price);							
+                }
 			}
 		}
 	}
@@ -36,18 +43,25 @@ namespace ADOPM3_02_19
         static void Main(string[] args)
         {
 			var stock = new Stock("MSFT");
-			stock.PriceChanged += ReportPriceChange;
 			stock.Price = 123;
+			stock.PriceChanged += ReportPriceChange;
+			stock.PriceChangedMoreThan20 += ReportPriceChangeMoreThan20;
 			stock.Price = 456;
-		}	
-		
+            Console.WriteLine(stock.Price);
+		}
+
 		static void ReportPriceChange(decimal oldPrice, decimal newPrice) //Subscriber eventhandler implementation
 		{
 			Console.WriteLine($"Price changed from {oldPrice} to {newPrice}");
 		}
+		static void ReportPriceChangeMoreThan20(decimal oldPrice, decimal newPrice) //Subscriber eventhandler implementation
+		{
+			Console.WriteLine($"Sell");
+		}
 	}
 
 	//Exercises
-	//1. Add two events that fires if the price increased/decreased more than 20%. These events should fire in addition to PriceChanged event
+	//1. Add two events that fires if the price increased/decreased more than 20%.
+	//   These events should fire in addition to PriceChanged event
 	//2. Add yet another event that loggs the price change in a file using streams
 }
