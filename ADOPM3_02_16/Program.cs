0a1a54538aef76d97ea0b7d16c990f0b5470f544
Martin Lenart <martin_lenart@icloud.com>
2022-12-05 09:39:59 +0100

    update

--- ADOPM3_02_16/Program.cs
+++ ADOPM3_02_16/Program.cs
@@ -10,8 +10,23 @@ namespace ADOPM3_02_16
         static void Main(string[] args)
         {
             AlarmResponse CompanyResponse = AlarmResponseCompany1;  // Create delegate object - shorthand
-            string response = CompanyResponse(1); // Invoke delegate - shorthand
-            Console.WriteLine(response); //
+            //string response = CompanyResponse(1); // Invoke delegate - shorthand
+            string response = CompanyResponse.Invoke(1); // Invoke delegate - longhand
+
+            Console.WriteLine(response);
+
+            /*
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                    CompanyResponse = AlarmResponseCompany1;
+                else
+                    CompanyResponse = AlarmResponseCompany2;
+
+                Console.WriteLine(CompanyResponse(2));
+            }
+            */
         }
 
         //The function that execution is delegated to
@@ -23,5 +38,13 @@ namespace ADOPM3_02_16
                 3 => "Company1 Easy Level",
                 _ => "Comapny1 Unknown Level"
             };
+        static string AlarmResponseCompany2(int priority) =>
+        (priority) switch
+        {
+            1 => "Company2 Critical Level",
+            2 => "Company2 Moderate Level",
+            3 => "Company2 Easy Level",
+            _ => "Comapny2 Unknown Level"
+        };
     }
 }
