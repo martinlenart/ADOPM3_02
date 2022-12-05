0659e8038f17977ef149e16b45b62e45d24db3e6
Martin Lenart <martin_lenart@icloud.com>
2022-12-05 12:51:19 +0100

    update

--- ADOPM3_02_16/Program.cs
+++ ADOPM3_02_16/Program.cs
@@ -10,23 +10,37 @@ namespace ADOPM3_02_16
         static void Main(string[] args)
         {
             AlarmResponse CompanyResponse = AlarmResponseCompany1;  // Create delegate object - shorthand
-            //string response = CompanyResponse(1); // Invoke delegate - shorthand
-            string response = CompanyResponse.Invoke(1); // Invoke delegate - longhand
+            string response = CompanyResponse(1); // Invoke delegate - shorthand
+
+            //string response = CompanyResponse.Invoke(1); // Invoke delegate - longhand
 
             Console.WriteLine(response);
 
-            /*
+            
             Console.WriteLine();
+            string r = null;
             for (int i = 0; i < 10; i++)
             {
                 if (i % 2 == 0)
+                    //r = AlarmResponseCompany1(3);
                     CompanyResponse = AlarmResponseCompany1;
                 else
+                    //r = AlarmResponseCompany2(3);
                     CompanyResponse = AlarmResponseCompany2;
 
-                Console.WriteLine(CompanyResponse(2));
+                //
+
+                r = ComplexResponse(CompanyResponse);
             }
-            */
+
+        }
+
+        private static string ComplexResponse(AlarmResponse CompanyResponse)
+        {
+            //complex algorithm, text ringa SOS Alarm
+            string r = CompanyResponse(3);
+            Console.WriteLine(r);
+            return r;
         }
 
         //The function that execution is delegated to
